654d664d3f84a87f3c6ba3d0d10991f29bd324fa
moxi-06 <monish280720@gmail.com>
2025-11-09 03:35:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,20 +55,6 @@ def keep_alive():
 
 threading.Thread(target=keep_alive, daemon=True).start()
 
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "I'm alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-Thread(target=run, daemon=True).start()
-
 async def Lucy_start():
     print('\n')
     print('\nInitalizing Lucy')
